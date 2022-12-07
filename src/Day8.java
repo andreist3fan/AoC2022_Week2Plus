39aacb13b7b6d440fa9ad72876ddcbe1d46ec864
andrei <and18ste@gmail.com>
2022-12-07 11:46:45 +0100

    Initial commit (with day 8 template)

---
+++ src/Day8.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day8 {
+
+    public static long solve1() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("input8.txt"));
+        return 0;
+    }
+    public static long solve2() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("input8.txt"));
+        return 0;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Day8.solve1());
+    }
+}
