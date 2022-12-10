3e1b4d7cd0a91f03cc572237f9add5054c90256c
andrei <and18ste@gmail.com>
2022-12-10 07:20:57 +0100

    added day 10

--- src/Day10.java
+++ src/Day10.java
@@ -3,12 +3,72 @@ import java.util.*;
 
 public class Day10 {
 
+    /**
+     *  10th day, first part
+     *  very easy and fun
+     * @return answer
+     * @throws FileNotFoundException if no file :(
+     */
     public static long solve1() throws FileNotFoundException {
         Scanner sc = new Scanner(new File("input10.txt"));
-        return 0;
+        long regX=1,ins=1,res=0;
+        while(sc.hasNextLine()){
+            if(ins%40==20 && ins <240){
+                res+=(ins*regX);
+            }
+            String[] inst = sc.nextLine().split(" ");
+            switch (inst[0]){
+                case "noop"->ins++;
+
+                case "addx"->{
+                    long val =Long.parseLong(inst[1]);
+                    ins++;
+                    if(ins%40==20 && ins <240){
+                        res+=(ins*regX);
+                    }
+                    regX+=val;
+                    ins++;
+                }
+            }
+        }
+        return res;
     }
-    public static long solve2() throws FileNotFoundException {
+
+    /**
+     * 10th day, second part
+     * <p>More fun, a bit less easy, but still easy-ish</p>
+     * @throws FileNotFoundException if no file :/
+     */
+    public static void solve2() throws FileNotFoundException {
         Scanner sc = new Scanner(new File("input10.txt"));
-        return 0;
+        char[][] pixel = new char[6][40];
+        long regX=1,ins=1;
+        while(sc.hasNextLine()){
+            if((ins-1)%40>=regX-1 && (ins-1)%40<=regX+1){
+                pixel[(int) ((ins-1)/40)][(int) ((ins-1)%40)]='#';
+            }
+            else pixel[(int) ((ins-1)/40)][(int) ((ins-1)%40)]='.';
+            String[] inst = sc.nextLine().split(" ");
+            switch (inst[0]){
+                case "noop"->ins++;
+
+                case "addx"->{
+                    long val =Long.parseLong(inst[1]);
+                    ins++;
+                    if((ins-1)%40>=regX-1 && (ins-1)%40<=regX+1){
+                        pixel[(int) ((ins-1)/40)][(int) ((ins-1)%40)]='#';
+                    }
+                    else pixel[(int) ((ins-1)/40)][(int) ((ins-1)%40)]='.';
+                    regX+=val;
+                    ins++;
+                }
+            }
+        }
+        for(int i=0;i<6;i++){
+            for(int j=0;j<40;j++){
+                System.out.print(pixel[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import java.io.*;
 import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println(Day9.solve2());
+        Day10.solve2();
     }
 }
